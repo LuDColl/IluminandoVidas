0ce6b94cafe87e3d2dcf9f43b1a6d9812f6d77ff
lgoliveira <lucas.guimaraes.lc@gmail.com>
2024-04-01 21:26:46 -0300

    Init Prontuario

---
+++ src/colors.tsx
@@ -0,0 +1,4 @@
+export const primaryColor = '#FE8B2C';
+export const secondaryColor = '#FFD31E';
+export const backgroundColor = '#FFFFFF';
+export const textColor = '#272324';
--- src/screens/home/home.styles.tsx
+++ src/screens/home/home.styles.tsx
@@ -1,7 +1,48 @@
-import { StyleProp, ViewStyle } from 'react-native';
+import { textColor } from 'colors';
+import { ImageStyle, StyleProp, TextStyle, ViewStyle } from 'react-native';
 
-export const viewStyle: StyleProp<ViewStyle> = {
+export const screenStyle: StyleProp<ViewStyle> = {
   flex: 1,
-  justifyContent: 'center',
+  justifyContent: 'flex-start',
   alignItems: 'center',
+  flexDirection: 'column',
+  padding: 16,
+};
+
+export const identificationStyle: StyleProp<ViewStyle> = {
+  flexDirection: 'row',
+  alignItems: 'flex-end',
+  width: '100%',
+};
+
+export const identificationDataStyle: StyleProp<ViewStyle> = {
+  paddingLeft: 16,
+};
+
+export const imgageStyle: StyleProp<ImageStyle> = { width: 150, height: 150 };
+export const faceStyle: StyleProp<ImageStyle> = {
+  ...imgageStyle,
+  borderRadius: 8,
+};
+
+export const textStyle: StyleProp<TextStyle> = { color: textColor };
+export const nextTextStyle: StyleProp<TextStyle> = {
+  ...textStyle,
+  marginTop: 8,
+};
+export const startTextStyle: StyleProp<TextStyle> = {
+  ...textStyle,
+  alignSelf: 'flex-start',
+};
+
+export const hrStyle: StyleProp<ViewStyle> = {
+  backgroundColor: textColor,
+  height: 1,
+  width: '100%',
+  marginVertical: 16,
+};
+
+export const halfHrStyle: StyleProp<ViewStyle> = {
+  ...hrStyle,
+  marginVertical: 8,
 };
