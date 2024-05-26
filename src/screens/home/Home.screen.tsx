54ec4897ab309a5812eeccede25eb683c389ccda
Augusto21 <camolesi2121@hotmail.com>
2024-05-26 19:35:23 -0300

    Adicionando tela de cadastro cliente.

--- src/screens/home/Home.screen.tsx
+++ src/screens/home/Home.screen.tsx
@@ -3,6 +3,7 @@ import TabBarComponent from 'components/TabBar.component';
 import { View } from 'react-native';
 import { Icon } from 'react-native-paper';
 import StudentsScreen from 'screens/students/Students.screen';
+import CadastroCliente from 'screens/cadastro/CadastroCliente.screen'
 
 const Tab = createBottomTabNavigator();
 
@@ -24,6 +25,19 @@ export default function HomeScreen() {
           },
         }}
       />
+      <Tab.Screen
+                          name="cadastro"
+                          component={CadastroCliente}
+                          options={{
+                            tabBarLabel: 'Cadastrar Usuario',
+                            tabBarIcon: ({ color, size }) => {
+                              return <Icon source="format-list-bulleted-square" 
+                              size={size} 
+                              color={color} 
+                              />;
+                            },
+                          }}
+                        />
       <Tab.Screen
         name="students"
         component={StudentsScreen}
