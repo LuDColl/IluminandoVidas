2356a22824f4b88f8c0227f027a9939c2b2b2838
lgoliveira <lucas.guimaraes.lc@gmail.com>
2024-05-27 23:47:12 -0300

    develop Resolve Error Handing of Search

--- src/screens/students/Students.screen.tsx
+++ src/screens/students/Students.screen.tsx
@@ -4,9 +4,12 @@ import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from 'router';
 import SearchComponent from 'components/Search.component';
+import SnackbarContextComponet from 'components/SnackbarContext.component';
 
 export default function StudentsScreen() {
   const [students, setStudents] = useState<any[] | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
   const navigation =
     useNavigation<NativeStackNavigationProp<RootStackParamList, 'Students'>>();
 
@@ -15,23 +18,26 @@ export default function StudentsScreen() {
   }, []);
 
   const getStudents = async () => {
-    const students = await supabase
+    const { data, error } = await supabase
       .from('aluno')
       .select('id_aluno, str_nomealuno');
 
-    setStudents(students.data ?? []);
+    setStudents(data ?? []);
+    if (error) setError('Erro ao carregar alunos');
   };
 
   return (
-    <SearchComponent
-      items={students}
-      getKey={({ id_aluno }) => id_aluno}
-      getText={({ str_nomealuno }) => str_nomealuno}
-      noData="Nenhum aluno encontrado"
-      onPress={({ id_aluno }) => navigation.push('Student', { id: id_aluno })}
-      onRefresh={getStudents}
-      placeholder="Digite o nome do aluno"
-      icon="account-circle"
-    />
+    <SnackbarContextComponet setMessage={setError} message={error}>
+      <SearchComponent
+        items={students}
+        getKey={({ id_aluno }) => id_aluno}
+        getText={({ str_nomealuno }) => str_nomealuno}
+        noData="Nenhum aluno encontrado"
+        onPress={({ id_aluno }) => navigation.push('Student', { id: id_aluno })}
+        onRefresh={getStudents}
+        placeholder="Digite o nome do aluno"
+        icon="account-circle"
+      />
+    </SnackbarContextComponet>
   );
 }
