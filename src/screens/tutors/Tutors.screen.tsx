c5c6b43e20c9657182eb96e74b71a53951947b3c
lgoliveira <lucas.guimaraes.lc@gmail.com>
2024-05-27 23:47:12 -0300

    develop Resolve Error Handing of Search

--- src/screens/tutors/Tutors.screen.tsx
+++ src/screens/tutors/Tutors.screen.tsx
@@ -4,9 +4,12 @@ import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from 'router';
 import SearchComponent from 'components/Search.component';
+import SnackbarContextComponet from 'components/SnackbarContext.component';
 
 export default function TutorsScreen() {
   const [tutors, setTutors] = useState<any[] | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
   const navigation =
     useNavigation<NativeStackNavigationProp<RootStackParamList, 'Students'>>();
 
@@ -15,20 +18,26 @@ export default function TutorsScreen() {
   }, []);
 
   const getTutors = async () => {
-    const students = await supabase.from('tutor').select('id_tutor, str_nome');
-    setTutors(students.data ?? []);
+    const { data, error } = await supabase
+      .from('tutor')
+      .select('id_tutor, str_nome');
+
+    setTutors(data ?? []);
+    if (error) setError('Erro ao carregar tutores');
   };
 
   return (
-    <SearchComponent
-      items={tutors}
-      getKey={({ id_tutor }) => id_tutor}
-      getText={({ str_nome }) => str_nome}
-      noData="Nenhum tutor encontrado"
-      onPress={({ id_tutor }) => navigation.push('Tutor', { id: id_tutor })}
-      onRefresh={getTutors}
-      placeholder="Digite o nome do tutor"
-      icon="account-circle"
-    />
+    <SnackbarContextComponet setMessage={setError} message={error}>
+      <SearchComponent
+        items={tutors}
+        getKey={({ id_tutor }) => id_tutor}
+        getText={({ str_nome }) => str_nome}
+        noData="Nenhum tutor encontrado"
+        onPress={({ id_tutor }) => navigation.push('Tutor', { id: id_tutor })}
+        onRefresh={getTutors}
+        placeholder="Digite o nome do tutor"
+        icon="account-circle"
+      />
+    </SnackbarContextComponet>
   );
 }
