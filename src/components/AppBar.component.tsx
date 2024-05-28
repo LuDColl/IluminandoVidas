7f54989a42cdb8c9196f11327d88473c8b255f0c
lgoliveira <lucas.guimaraes.lc@gmail.com>
2024-05-28 18:31:14 -0300

    correcoes Resolve List View

--- src/components/AppBar.component.tsx
+++ src/components/AppBar.component.tsx
@@ -1,15 +1,22 @@
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { ReactNode } from 'react';
+import { Animated, StyleProp, ViewStyle } from 'react-native';
 import { Appbar } from 'react-native-paper';
 import { RootStackParamList } from 'router';
 
-export default function AppbarComponent({ title }: { title: ReactNode }) {
+export default function AppbarComponent({
+  title,
+  style,
+}: {
+  title: ReactNode;
+  style?: Animated.WithAnimatedValue<StyleProp<ViewStyle>>;
+}) {
   const navigation =
     useNavigation<NativeStackNavigationProp<RootStackParamList>>();
 
   return (
-    <Appbar.Header>
+    <Appbar.Header style={style}>
       <Appbar.BackAction onPress={() => navigation.pop()} />
       <Appbar.Content title={title} />
     </Appbar.Header>
